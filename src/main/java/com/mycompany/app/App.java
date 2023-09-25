62c2804109c944689ccb388bda961e060fb3013c
xsantq <37873784+xsantq@users.noreply.github.com>
2023-09-25 19:57:10 +0300

    son olsun

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Cemmmmmmmmmmmmmmmm Varolllllllll!" );
+        System.out.println( "Hadi Paşammm!" );
     }
 }
